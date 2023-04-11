1a8218e8d72cb237db86a4a62dca849327c39577
Sergio Moura <sergio@moura.ca>
2023-04-11 12:00:51 -0400

    protect endpoint with a static secret

--- api/import-all-movies.go
+++ api/import-all-movies.go
@@ -6,9 +6,25 @@ import (
 	"github.com/lsmoura/omdb-api/logging"
 	"github.com/lsmoura/omdb-api/omdb"
 	"net/http"
+	"os"
 )
 
 func APIImportAllMovies(w http.ResponseWriter, r *http.Request) {
+	// protect the endpoint with a secret
+	requiredSecret := os.Getenv("OMDB_SECRET")
+	if requiredSecret == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprintf(w, "Error: OMDB_SECRET not set")
+		return
+	}
+
+	auth := r.URL.Query().Get("auth")
+	if auth != requiredSecret {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintf(w, "Error: unauthorized")
+		return
+	}
+
 	logging.LoggerMiddleware(http.HandlerFunc(importAllMoviesHandler), nil).ServeHTTP(w, r)
 }
 
